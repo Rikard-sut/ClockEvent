a6b983bb4b61653baa3a900f0a93d6903deae596
Rikard-Sut <mailarikardskoglundnu@gmail.com>
2020-03-26 16:01:28 +0100

    ConsoleoutPut format fix

--- Program/Display.cs
+++ Program/Display.cs
@@ -10,9 +10,9 @@ namespace Program
     {
         public static void PrintTime(object source, Clock.ClockEventArgs e)
         {
-            Clock clock = (Clock)source; 
-            Console.WriteLine("Tid = H{0}, M{1}, S{2}", e.CurrentTime.Hour, e.CurrentTime.Minute, e.CurrentTime.Second);
-
+            Clock clock = (Clock)source;
+            Console.Clear();
+            Console.WriteLine("Logging to file, CurrentTime = {0}:{1}:{2}", e.CurrentTime.Hour, e.CurrentTime.Minute, e.CurrentTime.Second);
         }
     }
 }
